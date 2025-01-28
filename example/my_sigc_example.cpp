13f564ddf7412b5784109dd498386dbc1cd27671
huanzze <2805033624@qq.com>
2025-01-28 11:06:14 +0800

    Re-encapsulate the API of sigc++, and code a example of sigc++-2.2.11.

---
+++ example/my_sigc_example.cpp
@@ -0,0 +1,48 @@
+#include "predefine.h"
+
+void simple_callback(){
+    std::cout<< "Simple triggered!" << std::endl;
+}
+
+void simple_callback_with_arg(int arg){
+    std::cout<< "Simple triggered with arg: " << arg << std::endl;
+}
+
+void simple_callback_with_arg_and_return(int &ret, int arg){
+    std::cout<< "Simple triggered with arg: " << arg << std::endl;
+    ret = arg;
+}
+
+class Receiver{
+    public:
+        void member_callback(int value){
+            std::cout<< "Member triggered!: " << value << std::endl;
+        }
+};
+int main(){
+
+    Signal_void signal_simple;
+    auto slot_simple = Slot_void(Ptr_fun(simple_callback));
+    signal_simple.connect(slot_simple);
+    signal_simple.emit();
+
+    Signal_int signal_simple_with_arg;
+    auto slot_simple_with_arg = Slot_int(Ptr_fun(simple_callback_with_arg));
+    signal_simple_with_arg.connect(slot_simple_with_arg);
+    signal_simple_with_arg.emit(42);
+
+    Signal_reint_int signal_simple_with_arg_return;
+    auto slot_simple_with_arg_return = Slot_reint_int(Ptr_fun(simple_callback_with_arg_and_return));
+    signal_simple_with_arg_return.connect(slot_simple_with_arg_return);
+    int ret;
+    signal_simple_with_arg_return.emit(ret, 52);
+    std::cout << "simple_callback_with_arg_and_return: " << ret << std::endl;
+
+    Receiver receiver;
+    Signal_int signal_class_with_arg_receiver;
+    auto slot_class_with_arg_receiver = Slot_int(Mem_fun(receiver, Receiver::member_callback));
+    signal_class_with_arg_receiver.connect(slot_class_with_arg_receiver);
+    signal_class_with_arg_receiver.emit(62);
+
+    return 0;
+}
--- include/predefine.h
+++ include/predefine.h
@@ -1,17 +1,37 @@
-//
-// Created by huanzze on 25-1-26.
-//
-
 #ifndef PREDEFINE_H
 #define PREDEFINE_H
 
 #include <sigc++/sigc++.h>
-// #include <sigc++.h>
 #include <iostream>
+/*
+ * The encapsulation of sigc++
+*/
+// no return Signal_type
+typedef sigc::signal<void>								Signal_void;
+typedef sigc::signal<void, int>             			Signal_int;
+typedef sigc::signal<void, std::string>     			Signal_string;
+// return Signal_type
+typedef sigc::signal<void, int&> 						Signal_reint_void;
+typedef sigc::signal<void, int&, int> 					Signal_reint_int;
+typedef sigc::signal<void, int&, std::string> 			Signal_reint_string;
+typedef sigc::signal<void, std::string&, std::string> 	Signal_restring_string;
+// no return Slot_type
+typedef sigc::slot<void> 								Slot_void;
+typedef sigc::slot<void, int> 							Slot_int;
+typedef sigc::slot<void, std::string> 					Slot_string;
+// return Slot_type
+typedef sigc::slot<void, int&>							Slot_reint_void;
+typedef sigc::slot<void, int&, int> 					Slot_reint_int;
+typedef sigc::slot<void, int&, std::string> 			Slot_reint_string;
+typedef sigc::slot<void, std::string&, std::string> 	Slot_restring_string;
+// package function
+#define Ptr_fun(x)          							sigc::ptr_fun(&x)
+#define Mem_fun(this, x)    							sigc::mem_fun(&this, &x)
 
-typedef sigc::signal<void> Signal_void;
-typedef sigc::signal<void(int)> Signal_int;
-typedef sigc::signal<void(std::string)> Signal_string;
+SIGC_USING_STD(cout)
+SIGC_USING_STD(endl)
+SIGC_USING_STD(string)
+/* end of sigc++ */
 
 
 
