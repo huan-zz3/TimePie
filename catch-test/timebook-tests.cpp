1e387fe59465ff175219e1f6e0688b490e3d7e63
huanzze <2805033624@qq.com>
2025-04-07 21:21:06 +0800

    Enable catch-test and optimize timebook-tests structure

--- catch-test/timebook-tests.cpp
+++ catch-test/timebook-tests.cpp
@@ -1,10 +1,8 @@
 #include "catch.hpp"
 
 #include "predefine.h"
-#include "itimebook.h"
-#include "timebook.h"
+#include "timebook/timebook.h"
 #include "ML307R-DL.h"
-#include "idatabase.h"
 #include "sqlite3db.h"
 
 #define ACCOUNT "2805033624@qq.com"
@@ -12,32 +10,92 @@
 
 TEST_CASE("dtu收发测试", "[uart]")
 {
-    std::shared_ptr<I4GDTU> dtu = std::static_pointer_cast<I4GDTU>(std::make_shared<ML307R>());
-    std::shared_ptr<ITimeBook> timebook = std::static_pointer_cast<ITimeBook>(std::make_shared<TimeBook>(ACCOUNT, PASSWORD));
-    auto serial = std::make_unique<serialib>();
-    std::shared_ptr<IDatabase> database = std::static_pointer_cast<IDatabase>(std::make_shared<Sqlite3Database>());
+    std::shared_ptr<ML307R> dtu = std::static_pointer_cast<ML307R>(std::make_shared<ML307R>());
+    std::shared_ptr<TimeBook> timebook = std::static_pointer_cast<TimeBook>(std::make_shared<TimeBook>(ACCOUNT, PASSWORD));
+    // auto serial = std::make_unique<serialib>();
+    std::shared_ptr<Sqlite3Database> database = std::static_pointer_cast<Sqlite3Database>(std::make_shared<Sqlite3Database>());
 
-    SECTION("setI4GDTU test")
+    // SECTION("setI4GDTU test")
+    // {
+    //     auto _rt = dtu->dtuInit();
+    //     REQUIRE(_rt.isSuccess() == true);
+    //     if (_rt.isSuccess() == false)
+    //     {
+    //         INFO(_rt.errormsg());
+    //     }
+
+    //     _rt = timebook->setI4GDTU(std::move(dtu));
+    //     REQUIRE(_rt.isSuccess() == true);
+    //     if (_rt.isSuccess() == false)
+    //     {
+    //         INFO(_rt.errormsg());
+    //     }
+
+    //     // dtu->dtuExit();
+    //     timebook.reset();
+    // }
+    // SECTION("timebook login test"){
+    //     auto _rt = dtu->dtuInit();
+    //     REQUIRE(_rt.isSuccess() == true);
+    //     if (_rt.isSuccess() == false)
+    //     {
+    //         INFO(_rt.errormsg());
+    //     }
+
+    //     _rt = timebook->setI4GDTU(std::move(dtu));
+    //     REQUIRE(_rt.isSuccess() == true);
+    //     if (_rt.isSuccess() == false)
+    //     {
+    //         INFO(_rt.errormsg());
+    //     }
+
+    //     _rt = timebook->timebookLogin();
+    //     REQUIRE(_rt.isSuccess() == true);
+    //     if (_rt.isSuccess() == false)
+    //     {
+    //         INFO(_rt.errormsg());
+    //     }
+
+    //     // dtu->dtuExit();
+    //     timebook.reset();
+    // }
+    // SECTION("timebook additem test"){
+    //     auto _rt = dtu->dtuInit();
+    //     REQUIRE(_rt.isSuccess() == true);
+    //     if (_rt.isSuccess() == false)
+    //     {
+    //         INFO(_rt.errormsg());
+    //     }
+
+    //     _rt = timebook->setI4GDTU(std::move(dtu));
+    //     REQUIRE(_rt.isSuccess() == true);
+    //     if (_rt.isSuccess() == false)
+    //     {
+    //         INFO(_rt.errormsg());
+    //     }
+
+    //     _rt = timebook->setIDatabase(std::move(database));
+    //     REQUIRE(_rt.isSuccess() == true);
+    //     if (_rt.isSuccess() == false)
+    //     {
+    //         INFO(_rt.errormsg());
+    //     }
+
+    //     auto _rt2 = timebook->addTimeItem( 1, "std::string _date", "std::string _start", "std::string _end", 66);
+    //     REQUIRE(_rt2.isSuccess() == true);
+    //     if (_rt2.isSuccess() == false)
+    //     {
+    //         INFO(_rt2.errormsg());
+    //     }else{
+    //         std::cout << _rt2.successvalue() << std::endl;
+    //     }
+
+    //     // dtu->dtuExit();
+    //     timebook.reset();
+    // }
+    SECTION("timebook getitem test")
     {
-        auto _rt = dtu->dtuInit(std::move(serial));
-        REQUIRE(_rt.isSuccess() == true);
-        if (_rt.isSuccess() == false)
-        {
-            INFO(_rt.errormsg());
-        }
-
-        _rt = timebook->setI4GDTU(std::move(dtu));
-        REQUIRE(_rt.isSuccess() == true);
-        if (_rt.isSuccess() == false)
-        {
-            INFO(_rt.errormsg());
-        }
-
-        // dtu->dtuExit();
-        timebook.reset();
-    }
-    SECTION("timebook login test"){
-        auto _rt = dtu->dtuInit(std::move(serial));
+        auto _rt = dtu->dtuInit();
         REQUIRE(_rt.isSuccess() == true);
         if (_rt.isSuccess() == false)
         {
@@ -58,24 +116,6 @@ TEST_CASE("dtu收发测试", "[uart]")
             INFO(_rt.errormsg());
         }
 
-        // dtu->dtuExit();
-        timebook.reset();
-    }
-    SECTION("timebook additem test"){
-        auto _rt = dtu->dtuInit(std::move(serial));
-        REQUIRE(_rt.isSuccess() == true);
-        if (_rt.isSuccess() == false)
-        {
-            INFO(_rt.errormsg());
-        }
-
-        _rt = timebook->setI4GDTU(std::move(dtu));
-        REQUIRE(_rt.isSuccess() == true);
-        if (_rt.isSuccess() == false)
-        {
-            INFO(_rt.errormsg());
-        }
-
         _rt = timebook->setIDatabase(std::move(database));
         REQUIRE(_rt.isSuccess() == true);
         if (_rt.isSuccess() == false)
@@ -83,16 +123,10 @@ TEST_CASE("dtu收发测试", "[uart]")
             INFO(_rt.errormsg());
         }
 
-        auto _rt2 = timebook->addTimeItem( 1, "std::string _date", "std::string _start", "std::string _end", 66);
+        auto _rt2 = timebook->getTimeItems();
         REQUIRE(_rt2.isSuccess() == true);
-        if (_rt2.isSuccess() == false)
-        {
-            INFO(_rt2.errormsg());
-        }else{
-            std::cout << _rt2.successvalue() << std::endl;
-        }
 
-        // dtu->dtuExit();
-        timebook.reset();
+        auto _rt3 = timebook->submitTimeItem("nothing");
+        REQUIRE(_rt3.isSuccess() == true);
     }
 }
