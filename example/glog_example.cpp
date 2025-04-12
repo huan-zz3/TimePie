949ee91231311932173a0e3647003705c774d387
huanzze <2805033624@qq.com>
2025-04-12 22:57:39 +0800

    Update glog example with improved logging flags and comments

--- example/glog_example.cpp
+++ example/glog_example.cpp
@@ -6,33 +6,31 @@
 #include <string>
 #include <glog/logging.h>
 
-int main(int argc, char** argv) {
-    FLAGS_alsologtostderr = 1;
+int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
 
-    //通过SetLogDestination可能没有设置log_dir标志位的方式方便(会遗漏一些日志)
-    //google::SetLogDestination(google::GLOG_INFO, "/tmp/today");
+    // 通过SetLogDestination可能没有设置log_dir标志位的方式方便(会遗漏一些日志)
+    // google::SetLogDestination(google::GLOG_INFO, "/tmp/today");
 
-    //标志位
-    FLAGS_colorlogtostderr=true;  //设置输出颜色
-    // FLAGS_v = std::atoi(argv[1]); //设置最大显示等级(超过该等级将不记录log)
+    // 标志位
+    FLAGS_alsologtostderr = 1;
+    FLAGS_colorlogtostderr = true; // 设置输出颜色
+    FLAGS_v = 3;                   // 设置自定义日志最大显示等级(超过该等级将不记录log)
     FLAGS_log_dir = "./logs";
 
-    LOG(INFO) << "Found " << google::COUNTER << " arguments!";
-
-    // assert
-    // CHECK(access(argv[2], 0) != -1) << "No such file: "<<argv[2];
+    LOG(INFO) << "Found " << google::COUNTER << " arguments!"; // google::COUNTER是glog库定义的全局变量，记录了所有的日志条数（不限级别）
 
     LOG(INFO) << "I am INFO!";
     LOG(WARNING) << "I am WARNING!";
     LOG(ERROR) << "I am ERROR!";
 
-    //VLOG用来自定义日志, 可以在括号内指定log级别
+    // VLOG用来自定义日志, 可以在括号内指定log级别
     VLOG(1) << "[Custom log(VLOG)] Level 1!";
     VLOG(2) << "[Custom log(VLOG)] Level 2!";
     VLOG(3) << "[Custom log(VLOG)] Level 3!";
     VLOG(4) << "[Custom log(VLOG)] Level 4! This is used for detailed message which need not to be printed each time.";
     VLOG(5) << "[Custom log(VLOG)] Level 5! On this level messages are print as detail as possible for debugging.";
+    
     LOG(FATAL) << "I am FATAL!";
     return 0;
 }
